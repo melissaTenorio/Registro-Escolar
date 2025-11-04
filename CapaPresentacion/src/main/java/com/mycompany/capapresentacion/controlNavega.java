be821f25d3129c528787d151788dfaf2b9b70ea5
melissaTenorio <melissa.tenorio247427@potros.itson.edu.mx>
2025-11-04 16:59:10 -0700

    Add files via upload

---
+++ CapaPresentacion/src/main/java/com/mycompany/capapresentacion/controlNavega.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.capapresentacion;
+
+/**
+ *
+ * @author melis
+ */
+public class controlNavega {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
