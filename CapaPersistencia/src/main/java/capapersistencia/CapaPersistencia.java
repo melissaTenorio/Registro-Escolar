e59a2289a6cbcf1daef0a598be7aa8a418b3f967
melissaTenorio <melissa.tenorio247427@potros.itson.edu.mx>
2025-11-04 17:08:35 -0700

    creacion de capas
    se crearon los proyectos de las capas en java

---
+++ CapaPersistencia/src/main/java/capapersistencia/CapaPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package capapersistencia;
+
+/**
+ *
+ * @author melis
+ */
+public class CapaPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
